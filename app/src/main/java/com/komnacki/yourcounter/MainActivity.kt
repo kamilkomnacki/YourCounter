6874ccf02e6c4484167e909f5b9317a8423d2fe9
Kamil Komnacki <kamilkomnacki@gmail.com>
2017-11-11 14:58:40 +0100

    Handle onLongClick(reset) event and orientation change.

--- app/src/main/java/com/komnacki/yourcounter/MainActivity.kt
+++ app/src/main/java/com/komnacki/yourcounter/MainActivity.kt
@@ -2,15 +2,26 @@ package com.komnacki.yourcounter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
-    var c = 0
-    var lc = 0
+
     var timer = Timer()
+    val TIME_PERIOD = 100
+    val updateHandler = Handler()
+    val runnable = Runnable { timerRun() }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,36 +30,39 @@ class MainActivity : AppCompatActivity() {
 
         linearlayout_main.setOnLongClickListener({longClick()})
         linearlayout_main.setOnClickListener({click()})
-
-
-        val COUNTER = object : Thread() {
-            override fun run() {
-
-                while (!isInterrupted) {
-                    try {
-                        //Counter frequence (in miliseconds)
-                        Thread.sleep(1000)
-                        runOnUiThread {
-                            timer.seconds += 1
-                            tv_seconds.text = timer.seconds.toString()
-                        }
-                    } catch (e: InterruptedException) {
-                        e.printStackTrace()
-                    }
-                }
-            }
-        }
-        COUNTER.start()
     }
 
-
-
     fun click(){
-        Toast.makeText(this, "Clicked! " + c, Toast.LENGTH_SHORT).show()
+        if(timer.isRun){
+            timer.isRun=false
+
+        }else{
+            timer.isRun = true
+            runnable.run()
+        }
+        Toast.makeText(this, "Click!", Toast.LENGTH_SHORT).show()
     }
 
     fun longClick() : Boolean{
-        Toast.makeText(this, "Long click! " + lc, Toast.LENGTH_LONG).show()
-        return false
+        timer.reset()
+        updateLayout()
+        Toast.makeText(this, "Long click!", Toast.LENGTH_SHORT).show()
+        return true
+    }
+
+
+    fun timerRun(){
+        if(timer.isRun){
+            updateHandler.postDelayed(runnable, TIME_PERIOD.toLong())
+            timer.add(TIME_PERIOD)
+            updateLayout()
+        }
+    }
+
+    fun updateLayout(){
+        if(tv_hours.visibility.equals(View.VISIBLE)) tv_hours.text = timer.hours.toString()
+        if(tv_minutes.visibility.equals(View.VISIBLE)) tv_minutes.text = timer.minutes.toString()
+        if(tv_seconds.visibility.equals(View.VISIBLE)) tv_seconds.text = timer.seconds.toString()
+        if(tv_miliseconds.visibility.equals(View.VISIBLE)) tv_miliseconds.text = timer.miliseconds.toString().substring(0,1)
     }
 }
--- app/src/main/java/com/komnacki/yourcounter/Timer.kt
+++ app/src/main/java/com/komnacki/yourcounter/Timer.kt
@@ -7,6 +7,37 @@ class Timer{
     var minutes = 0
     var seconds = 0
     var miliseconds = 0
+    var isRun = false
+
+    fun add(miliseconds:Int){
+        this.miliseconds += miliseconds
+        update()
+    }
+
+    fun reset(){
+        this.hours = 0
+        this.minutes = 0
+        this.seconds = 0
+        this.miliseconds = 0
+        isRun = false
+    }
+
+    private fun update(){
+        if(miliseconds>999){
+            miliseconds = 0
+            seconds += 1
+        }
+
+        if(seconds > 59){
+            seconds = 0
+            minutes += 1
+        }
+
+        if(minutes > 59){
+            minutes = 0
+            hours += 1
+        }
+    }
 
 
 
